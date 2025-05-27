30d72a155b636fa9735e77936976c23b8a7a98b8
i.pashegorov <matthew.kenway.mk@gmail.com>
2025-05-27 16:58:33 +0300

    xml parser decode number description of rights in text

--- xml_parse_test.py
+++ xml_parse_test.py
@@ -1,6 +1,29 @@
 import xml.etree.ElementTree as ET
 import os
 
+PERMISSIONS = {
+    0x1: 'Read Data / List Directory',
+    0x2: 'Write Data',
+    0x4: 'Append Data',
+    0x8: 'Read Extended Attributes',
+    0x10: 'Write Extended Attributes',
+    0x20: 'Execute / Traverse',
+    0x40: 'Delete Child',
+    0x80: 'Read Attributes',
+    0x100: 'Write Attributes',
+    0x10000: 'Delete',
+    0x20000: 'Read Permissions',
+    0x40000: 'Change Permissions',
+    0x80000: 'Take Ownership',
+    0x100000: 'Synchronize',
+}
+
+def decode_rights(rights_value: int) -> list[str]:
+    decoded = [name for bit, name in PERMISSIONS.items() if rights_value & bit]
+    if rights_value == 0x1F01FF:
+        decoded.append("FULL CONTROL")
+    return decoded
+
 def parse_folder_node(node, parent_path=""):
     folder_name_raw = node.findtext('Name')
     folder_name = os.path.join(parent_path, folder_name_raw) if parent_path else folder_name_raw
@@ -8,29 +31,27 @@ def parse_folder_node(node, parent_path=""):
 
     print(f"Папка: {folder_name}, Наследование: {folder_inherited}")
 
-    # Права на папку
     for perm in node.findall('./FolderPermissions/FolderPermission'):
         account = perm.findtext('./PrincipalDetails/AccountName')
-        rights = perm.findtext('Rights')
+        rights = int(perm.findtext('Rights'))
+        rights_text = decode_rights(rights)
         allow = perm.findtext('Allow') == 'true'
-        print(f"  Права на папку - {account}: {rights}, Allow: {allow}")
+        print(f"  Права на папку - {account}: {rights} ({', '.join(rights_text)}), Allow: {allow}")
 
-    # Права на файлы
     for fperm in node.findall('./FilePermissions/FilePermission'):
         fname_raw = fperm.findtext('Name')
         fname = os.path.join(folder_name, fname_raw)
         print(f"  Файл: {fname}")
         for diff in fperm.findall('./DiffPerms/DiffPerm'):
             account = diff.findtext('AccountName')
-            rights = diff.findtext('Rights')
+            rights = int(diff.findtext('Rights'))
+            rights_text = decode_rights(rights)
             allow = diff.findtext('Allow') == 'true'
-            print(f"    Права на файл - {account}: {rights}, Allow: {allow}")
+            print(f"    Права на файл - {account}: {rights} ({', '.join(rights_text)}), Allow: {allow}")
 
-    # Рекурсивный вызов
     for subnode in node.findall('./FolderNodes/FolderNode'):
         parse_folder_node(subnode, folder_name)
 
-# Парсинг XML-файла
 xml_file = 'PermissionsReport.xml'
 tree = ET.parse(xml_file)
 root = tree.getroot()
